a10f6ea68431f94a592bac682414d4bd40c2afd6
HangLe23 <20521286@gm.uit.edu.vn>
2023-11-24 16:59:17 +0700

    rename app

---
+++ UninstallProgram/Forms/UninstallerListDoubleClickAction.cs
@@ -0,0 +1,11 @@
+﻿using UninstallProgram.Properties;
+using Klocman.Localising;
+
+namespace UninstallProgram.Forms;
+
+public enum UninstallerListDoubleClickAction
+{
+    [LocalisedName(typeof (Localisable), nameof(Localisable.UninstallerListDoubleClickAction_DoNothing))] DoNothing = 0,
+    [LocalisedName(typeof (Localisable),nameof(Localisable.UninstallerListDoubleClickAction_Properties))] OpenProperties,
+    [LocalisedName(typeof (Localisable), nameof(Localisable.UninstallerListDoubleClickAction_Uninstall))] Uninstall
+}
